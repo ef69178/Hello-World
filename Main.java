eb68df64f9b57271e59198b63f37d69dfd636a6b
Evelyn Fu <69692036+ef69178@users.noreply.github.com>
2020-08-14 10:55:53 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Evelyn Fu");
+		System.out.println("I live the typical life of a boring student.");
+		System.out.println("I like to go biking with my friends.");
+		System.out.println("My favorite video game is WII, does that count?");
+		System.out.println("As for colleges, I don't know what I want to do and where I want to go.");
+	}
+}
